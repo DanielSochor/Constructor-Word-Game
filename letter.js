8f9bb0f64bf5283fdbaec7bc57161d532c16b74e
Daniel Sochor <47047939+DanielSochor@users.noreply.github.com>
2019-05-29 19:10:36 -0500

    Add files via upload
    Creates and displays a string of underscores for the word to guess

--- letter.js
+++ letter.js
@@ -1,20 +1,33 @@
 class Letter {
-    constructor(character, guessCharacter) {
-        console.log('letter is: '+character);
+    constructor(character) {
         this.character = character;
         this.guessed = false;
+        if (character == ' '){
+            this.guessed = true;
+        }
+        // if (this.guessed == false) {
+        //     //console.log('letter passed is: '+character)
+        //     console.log('_ ')
+        //     return 'a_ ';
+        // }
         this.uncoverCharacter = function () {
             if (this.guessed == true) {
                 return this.character;
             }
         }
-        this.correctGuess = function () {
-            if (!guessCharacter == '') {
-                if (guessCharacter == character) {
-                    this.guessed = true;
-                }
+        this.guess = function (value){
+            if(value == this.character){
+                this.guessed = true;
+                this.uncoverCharacter;
             }
         }
+        // this.correctGuess = function () {
+        //     if (!guessCharacter == '') {
+        //         if (guessCharacter == character) {
+        //             this.guessed = true;
+        //         }
+        //     }
+        // }
     }
 }
 
--- word.js
+++ word.js
@@ -3,14 +3,22 @@ var letter = require('./letter');
 class Word {
     constructor(word, guessed) {
         //console.log('passed in word is: ' + word)
-        this.word = [];
+        this.arrayOfLetterObjects = [];
+        this.displayWord = '';
         //array of `new` Letter objects representing the letters of the underlying word
+console.log('word to guess is: ' + word);
         this.wordString = function (word) {
             //console.log('passed in word is: ' + word)
             for (var i = 0; i < word.length; i++){
                 var character = new letter.letter(word[i]);
-                //console.log(word[i]);
+                this.arrayOfLetterObjects.push(character);
+                if(character.guessed == false){
+                    this.displayWord += '_ ';
+                } else {
+                    this.displayWord += '  ';
+                }
             }
+            console.log(this.displayWord);
         }
         //call the function on each letter object that displays letter or _ and concats
         this.guessOnEachLetter = function (character) {
